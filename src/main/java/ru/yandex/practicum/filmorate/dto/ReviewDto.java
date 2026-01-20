1e4930102832ff22174707d9ee6f3ad53e65cdcc
Dima <dimarastvorcev6123@gmail.com>
2026-01-20 18:49:10 +0300

    Refactor

--- src/main/java/ru/yandex/practicum/filmorate/dto/ReviewDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/ReviewDto.java
@@ -1,9 +1,6 @@
 package ru.yandex.practicum.filmorate.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import ru.yandex.practicum.filmorate.model.review.Review;
-
-import java.time.LocalDate;
 
 public record ReviewDto(
         @JsonProperty("reviewId")
