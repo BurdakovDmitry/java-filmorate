ee2348a81c20d5fb740a7c1e68d00b2356a1443d
Dima <dimarastvorcev6123@gmail.com>
2026-01-23 14:12:57 +0300

    Refactor add validation in EventService send method

--- src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
@@ -3,15 +3,18 @@ package ru.yandex.practicum.filmorate.service;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Event;
 import ru.yandex.practicum.filmorate.storage.event.EventDbStorage;
+import ru.yandex.practicum.filmorate.validation.Validation;
 
 import java.util.List;
 
 @Service
 public class EventService {
     private final EventDbStorage storage;
+    private final Validation validation;
 
-    public EventService(EventDbStorage storage) {
+    public EventService(EventDbStorage storage, Validation validation) {
         this.storage = storage;
+        this.validation = validation;
     }
 
     public void send(Event event) {
@@ -19,6 +22,8 @@ public class EventService {
     }
 
     public List<Event> getByUserId(Long userId) {
+        validation.userById(userId);
+
         return storage.getEvents(userId);
     }
 
