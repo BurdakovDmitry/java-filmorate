86d4f04dbc16907ae9b90afab307287ebcb6269c
Дмитрий Бурдаков <dmitryburdackov@yandex.ru>
2026-01-23 22:12:16 +0300

    Merge branch 'develop' into add-recommendations

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,11 @@
-MERGE INTO film_mpa (mpa_id, mpa_name)
-    KEY (mpa_id)
+INSERT INTO film_mpa (mpa_id, mpa_name)
     VALUES (1, 'G'),
            (2, 'PG'),
            (3, 'PG-13'),
            (4, 'R'),
            (5, 'NC-17');
 
-MERGE INTO genre (genre_id, name)
-    KEY (genre_id)
+INSERT INTO genre (genre_id, name)
     VALUES (1, 'Комедия'),
            (2, 'Драма'),
            (3, 'Мультфильм'),
